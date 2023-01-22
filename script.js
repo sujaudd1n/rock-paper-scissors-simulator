d7d2de6227590be6ef4a1c8d2639248101ad0587
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-01-22 20:28:15 +0530

    Add images and render random elements

--- script.js
+++ script.js
@@ -1,17 +1,47 @@
 const canvas = document.querySelector("#canvas");
 const FOOTER_HEIGHT = 40;
-let ctx = canvas.getContext("2d");
+const NUMBER = 5;
+const IMAGE_WIDTH = 40;
+const IMAGE_HEIGHT = IMAGE_WIDTH;
 
-render();
+let width = window.innerWidth;
+let height = window.innerHeight - FOOTER_HEIGHT;
+let ctx = canvas.getContext("2d");
 
 window.addEventListener("resize", render);
 
+function draw_image(path, x, y) {
+  const img = new Image();
+  img.addEventListener("load", () => {
+    ctx.drawImage(img, x, y);
+  });
+  img.src = path;
+}
+
 function render() {
-  let width = window.innerWidth;
-  let height = window.innerHeight - FOOTER_HEIGHT;
+  width = window.innerWidth;
+  height = window.innerHeight - FOOTER_HEIGHT;
 
   canvas.width = width;
   canvas.height = height;
-  ctx.fillStyle = "green";
-  ctx.fillRect(10, 10, width / 2, height / 2);
+
+  for (let i = 0; i < NUMBER; ++i) {
+    let x = Math.random() * width - IMAGE_WIDTH;
+    let y = Math.random() * height - IMAGE_HEIGHT;
+    x < 0 ? x = 0 : 1;
+    y < 0 ? y = 0 : 1;
+    draw_image("rock.svg", x, y);
+    x = Math.random() * width - IMAGE_WIDTH;
+    y = Math.random() * height - IMAGE_HEIGHT;
+    x < 0 ? x = 0 : 1;
+    y < 0 ? y = 0 : 1;
+    draw_image("paper.svg", x, y);
+    x = Math.random() * width - IMAGE_WIDTH;
+    y = Math.random() * height - IMAGE_HEIGHT;
+    x < 0 ? x = 0 : 1;
+    y < 0 ? y = 0 : 1;
+    draw_image("sissor.svg", x, y);
+  }
 }
+
+render();
