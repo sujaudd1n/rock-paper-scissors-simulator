1d2795074d5524cb1286030615fbf6de98239b44
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-02-03 13:18:01 +0530

    Implement collision among objects

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
-const ROCK = [];
-const PAPER = [];
-const SCISSOR = [];
+const SPRITES = [];
 for (let i = 0; i < NUMBER; ++i) {
-  ROCK.push(new Sprite("rock.svg"));
-  PAPER.push(new Sprite("paper.svg"));
-  SCISSOR.push(new Sprite("scissor.svg"));
+  SPRITES.push(new Sprite("rock.svg", "ROCK"));
+  SPRITES.push(new Sprite("paper.svg", "PAPER"));
+  SPRITES.push(new Sprite("scissor.svg", "SCISSOR"));
 }
+
+function change_sprite(a, b)
+{
+    let t = a.image.src;
+    a.image.src = b.image.src;
+    b.image.src = t;
+
+    a.change_direction_collide();
+    b.change_direction_collide();
+
+    a.x += 10*a.delta_x;
+    a.y += 10*a.delta_y;
+
+    b.x += 10*b.delta_x;
+    b.y += 10*b.delta_y;
+
+}
+
+function detect_collision()
+{
+    for (let i = 0; i < SPRITES.length - 1; ++i)
+    {
+        for (let j = i + 1; j < SPRITES.length; ++j)
+        {
+            let cx = Math.abs(SPRITES[i].x - SPRITES[j].x);
+            let cy = Math.abs(SPRITES[i].y - SPRITES[j].y);
+
+            if (Math.abs(SPRITES[i].x - SPRITES[j].x) <= IMAGE_WIDTH &&
+                Math.abs(SPRITES[i].y - SPRITES[j].y) <= IMAGE_HEIGHT)
+            {
+                console.log(i,j, "x = ", cx);
+                console.log(i,j, "y = ", cy);
+
+                change_sprite(SPRITES[i], SPRITES[j]);
+            }
+        }
+    }
+}
+
+setInterval(detect_collision, 10);
 
 function draw_all() {
   ctx.clearRect(0, 0, width, height);
 
-  for (let i = 0; i < NUMBER; ++i) {
-    ROCK[i].draw();
-    PAPER[i].draw();
-    SCISSOR[i].draw();
+  for (let i = 0; i < SPRITES.length; ++i) {
+    SPRITES[i].draw();
   }
-
   window.requestAnimationFrame(draw_all);
 }
 
--- utilities.js
+++ utilities.js
@@ -1,7 +1,7 @@
 const FOOTER_HEIGHT = 40;
-const NUMBER = 5;
+const NUMBER = 3
 const IMAGE_WIDTH = 40;
-const IMAGE_HEIGHT = IMAGE_WIDTH;
+const IMAGE_HEIGHT = 40;
 
 let width = window.innerWidth;
 let height = window.innerHeight - FOOTER_HEIGHT;
@@ -10,27 +10,34 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
 class Sprite {
-  constructor(path) {
+  constructor(path, type) {
     this.path = path;
-    this.x = Math.floor(Math.random() * width - IMAGE_WIDTH);
-    this.y = Math.floor(Math.random() * height - FOOTER_HEIGHT - IMAGE_HEIGHT);
+    this.x = Math.floor(Math.random() * (width - IMAGE_WIDTH));
+    this.y = Math.floor(Math.random() * (height - FOOTER_HEIGHT - IMAGE_HEIGHT));
     this.image = new Image();
     this.image.src = path;
     this.delta_x = 1.5;
     this.delta_y = 1.5;
+    this.type = type;
   }
-  draw() {
-    if (this.x >= width - IMAGE_WIDTH)
-        this.delta_x *= -1;
-    else if (this.x <= 0)
-        this.delta_x *= -1;
-    if (this.y >= height - IMAGE_HEIGHT)
-        this.delta_y *= -1;
-    else if (this.y <= 0)
-        this.delta_y *= -1;
-
+  change_direction() {
+    if (this.x >= width - IMAGE_WIDTH) this.delta_x *= -1;
+    else if (this.x <= 0) this.delta_x *= -1;
+    if (this.y >= height - IMAGE_HEIGHT) this.delta_y *= -1;
+    else if (this.y <= 0) this.delta_y *= -1;
+  }
+  change_direction_collide()
+  {
+    this.delta_x *= -1;
+    this.delta_y *= -1;
+  }
+  change_position() {
     this.x += this.delta_x;
     this.y += this.delta_y;
+  }
+  draw() {
+    this.change_direction();
+    this.change_position();
     ctx.drawImage(this.image, this.x, this.y);
   }
 }
