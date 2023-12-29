84690d34b7dbdef6347abcbc5c5fc2733bc4a5c5
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2023-12-29 10:52:41 +0530

    Improvements and bug fix

--- script.js
+++ script.js
@@ -1,81 +1,127 @@
-const SPRITES = [];
-for (let i = 0; i < NUMBER; ++i) {
-  SPRITES.push(new Sprite("rock.svg", "ROCK"));
-  SPRITES.push(new Sprite("paper.svg", "PAPER"));
-  SPRITES.push(new Sprite("scissor.svg", "SCISSOR"));
-}
-render();
-window.addEventListener("resize", render);
+function detect_win() {
+    let data = [0, 0, 0];
+    for (let item of SPRITES) {
+        if (item.type === "ROCK") data[0] = 1;
+        else if (item.type === "PAPER") data[1] = 1;
+        else data[2] = 1;
+    }
+    if (data[0] + data[1] + data[2] <= 2) {
+        let c1, c2;
+        if (data[0] === 0) {
+            c1 = "PAPER";
+            c2 = "SCISSOR";
+        }
 
-setInterval(detect_collision, 10);
-setInterval(detect_two, 10);
-function detect_two() {
-  let data = [0, 0, 0];
-  for (let item of SPRITES) {
-    if (item.type === "ROCK") data[0] = 1;
-    else if (item.type === "PAPER") data[1] = 1;
-    else data[2] = 1;
-  }
-  if (data[0] + data[1] + data[2] <= 2)
-    MAX_SPEED = 7;
+        if (data[1] === 0) {
+            c1 = "ROCK";
+            c2 = "SCISSOR";
+        }
+        if (data[2] === 0) {
+            c1 = "ROCK";
+            c2 = "PAPER";
+        }
+        let winner;
+        console.log(c1, c2);
+
+        if (c1 === "ROCK") {
+            if (c2 === "PAPER") winner = c2;
+            else winner = c1;
+        } else if (c1 === "PAPER") {
+            if (c2 === "ROCK") winner = c1;
+            else winner = c2;
+        } else if (c2 === "ROCK") {
+            winner = c2;
+        } else winner = c1;
+
+        if (winner) {
+            reset();
+            console.log(winner);
+            show_winner(winner);
+            return true;
+        }
+    }
+    return false;
+}
+
+function show_winner(winner) {
+    const elem = document.createElement("div");
+    elem.textContent = winner + " has won";
+    elem.classList.add("winnder-card");
+    document.body.append(elem);
+}
+
+let dwid;
+
+function detect_collision() {
+    for (let i = 0; i < SPRITES.length; ++i) {
+        for (let j = 0; j < SPRITES.length; ++j) {
+            if (SPRITES[i].type !== SPRITES[j].type) {
+                if (
+                    Math.abs(SPRITES[i].x - SPRITES[j].x) < IMAGE_WIDTH &&
+                    Math.abs(SPRITES[i].y - SPRITES[j].y) < IMAGE_HEIGHT
+                ) {
+                    change_sprite(SPRITES[i], SPRITES[j]);
+                    SPRITES[i].change_direction_and_motion();
+                    SPRITES[j].change_direction_and_motion();
+                    SPRITES[i].change_position(2);
+                    SPRITES[j].change_position(2);
+                    detect_win();
+                }
+            }
+        }
+    }
 }
 
 function change_sprite(a, b) {
-  if (a.type !== b.type) {
-    if (a.type == "ROCK" && b.type == "PAPER") {
-      a.type = "PAPER";
-      a.image.src = b.image.src;
-    } else if (a.type == "ROCK" && b.type == "SCISSOR") {
-      b.type = "ROCK";
-      b.image.src = a.image.src;
-    } else if (a.type == "PAPER" && b.type == "ROCK") {
-      b.type = "PAPER";
-      b.image.src = a.image.src;
-    } else if (a.type == "PAPER" && b.type == "SCISSOR") {
-      a.type = "SCISSOR";
-      a.image.src = b.image.src;
-    } else if (a.type == "SCISSOR" && b.type == "ROCK") {
-      a.type = "ROCK";
-      a.image.src = b.image.src;
-    } else {
-      b.type = "SCISSOR";
-      b.image.src = a.image.src;
+    if (a.type !== b.type) {
+        if (a.type == "ROCK" && b.type == "PAPER") {
+            a.type = "PAPER";
+            a.image.src = b.image.src;
+        } else if (a.type == "ROCK" && b.type == "SCISSOR") {
+            b.type = "ROCK";
+            b.image.src = a.image.src;
+        } else if (a.type == "PAPER" && b.type == "ROCK") {
+            b.type = "PAPER";
+            b.image.src = a.image.src;
+        } else if (a.type == "PAPER" && b.type == "SCISSOR") {
+            a.type = "SCISSOR";
+            a.image.src = b.image.src;
+        } else if (a.type == "SCISSOR" && b.type == "ROCK") {
+            a.type = "ROCK";
+            a.image.src = b.image.src;
+        } else {
+            b.type = "SCISSOR";
+            b.image.src = a.image.src;
+        }
     }
-
-    a.change_direction_on_collide();
-    b.change_direction_on_collide();
-
-    a.change_position();
-    b.change_position();
-  }
-}
-
-function detect_collision() {
-  for (let i = 0; i < SPRITES.length; ++i) {
-    for (let j = 0; j < SPRITES.length; ++j) {
-      if (i !== j) {
-        if (
-          Math.abs(SPRITES[i].x - SPRITES[j].x) < IMAGE_WIDTH &&
-          Math.abs(SPRITES[i].y - SPRITES[j].y) < IMAGE_HEIGHT
-        )
-          change_sprite(SPRITES[i], SPRITES[j]);
-      }
-    }
-  }
 }
 
 function draw_all() {
-  ctx.clearRect(0, 0, width, height);
-  for (let i = 0; i < SPRITES.length; ++i) SPRITES[i].draw();
-  window.requestAnimationFrame(draw_all);
+    ctx.clearRect(0, 0, width, height);
+    detect_collision();
+    for (let i = 0; i < SPRITES.length; ++i) SPRITES[i].update_and_draw();
+    if (!detect_win()) window.requestAnimationFrame(draw_all);
 }
 
 function render() {
-  width = window.innerWidth;
-  height = window.innerHeight - FOOTER_HEIGHT;
-
-  canvas.width = width;
-  canvas.height = height;
-
-  window.requestAnimationFrame(draw_all);
+    width = window.innerWidth;
+    height = window.innerHeight - FOOTER_HEIGHT;
+    canvas.width = width;
+    canvas.height = height;
 }
+
+function reset() {}
+
+const SPRITES = [];
+
+for (let i = 0; i < NUMBER; ++i) {
+    SPRITES.push(new Sprite("rock.svg", "ROCK"));
+    SPRITES.push(new Sprite("paper.svg", "PAPER"));
+    SPRITES.push(new Sprite("scissor.svg", "SCISSOR"));
+}
+
+render();
+let raid = window.requestAnimationFrame(draw_all);
+
+window.addEventListener("resize", render);
+// let dcid = setInterval(detect_collision, 10);
--- utilities.js
+++ utilities.js
@@ -1,8 +1,10 @@
+const states = ["ready", "playing", "end"];
+
 const NUMBER = 7;
 const FOOTER_HEIGHT = 40;
 const IMAGE_WIDTH = 40;
 const IMAGE_HEIGHT = 40;
-let MAX_SPEED = 1.5;
+let MAX_SPEED = 30.5;
 
 let width = window.innerWidth;
 let height = window.innerHeight - FOOTER_HEIGHT;
@@ -11,50 +13,53 @@ const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
 class Sprite {
-  constructor(path, type) {
-    this.path = path;
-    this.type = type;
-    this.x = this.get_x_position();
-    this.y = this.get_y_position();
-    this.delta_x = this.get_motion();
-    this.delta_y = this.get_motion();
-    this.image = new Image();
-    this.image.src = path;
-  }
-  get_x_position() {
-    return Math.floor(Math.random() * (width - IMAGE_WIDTH));
-  }
-  get_y_position() {
-    return Math.floor(Math.random() * (height - FOOTER_HEIGHT - IMAGE_HEIGHT));
-  }
-  get_motion() {
-    return (
-      (Math.random() * (MAX_SPEED - 1) + 1) * [1, -1][Math.floor(Math.random() * 2)]
-    );
-  }
-  draw() {
-    this.change_direction();
-    this.change_position();
-    ctx.drawImage(this.image, this.x, this.y);
-  }
-  change_direction() {
-    if (this.x + this.delta_x > width - IMAGE_WIDTH) {
-      this.delta_x *= -1;
-    } else if (this.x + this.delta_x < 0) {
-      this.delta_x *= -1;
+    constructor(path, type) {
+        this.type = type;
+        this.x = this.get_x_position();
+        this.y = this.get_y_position();
+        this.delta_x = this.get_motion();
+        this.delta_y = this.get_motion();
+        this.image = new Image();
+        this.image.src = path;
     }
-    if (this.y + this.delta_y > height - IMAGE_HEIGHT - FOOTER_HEIGHT) {
-      this.delta_y *= -1;
-    } else if (this.y + this.delta_y < 0) {
-      this.delta_y *= -1;
+    get_x_position() {
+        return Math.floor(Math.random() * (width - IMAGE_WIDTH));
     }
-  }
-  change_position() {
-    this.x += this.delta_x;
-    this.y += this.delta_y;
-  }
-  change_direction_on_collide() {
-    this.delta_x = this.get_motion();
-    this.delta_y = this.get_motion();
-  }
+    get_y_position() {
+        return Math.floor(
+            Math.random() * (height - FOOTER_HEIGHT - IMAGE_HEIGHT)
+        );
+    }
+    get_motion() {
+        return (
+            (Math.random() * (MAX_SPEED - 1) + 1) *
+            [1, -1][Math.floor(Math.random() * 2)]
+        );
+    }
+    update_and_draw() {
+        this.change_direction();
+        this.change_position();
+        ctx.drawImage(this.image, this.x, this.y);
+    }
+    change_direction() {
+        if (this.x + this.delta_x > width - IMAGE_WIDTH) {
+            this.delta_x *= -1;
+        } else if (this.x + this.delta_x < 0) {
+            this.delta_x *= -1;
+        }
+        if (this.y + this.delta_y > height - IMAGE_HEIGHT - FOOTER_HEIGHT) {
+            this.delta_y *= -1;
+        } else if (this.y + this.delta_y < 0) {
+            this.delta_y *= -1;
+        }
+    }
+    change_position(factor = 1) {
+        this.x += this.delta_x * factor;
+        this.y += this.delta_y * factor;
+    }
+    change_direction_and_motion() {
+        this.change_direction();
+        this.delta_x = this.get_motion();
+        this.delta_y = this.get_motion();
+    }
 }
